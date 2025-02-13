3dfcef23c08a977fe672174d8ef4fe73ea2a0917
cassido-fx <okaforcassy@gmail.com>
2025-02-13 09:32:02 +0100

    Initial commit - Flask CRUD app

---
+++ app.py
@@ -0,0 +1,67 @@
+# Imports
+from flask import Flask, render_template, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+# My app
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todolist.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    completed = db.Column(db.Boolean, default=False)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"Task {self.id}"
+
+@app.route("/", methods=['POST', 'GET'])
+def index():
+    # Add a task
+    if request.method == "POST":
+        current_task = request.form['content']
+        new_task = Task(content=current_task)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            print(f"ERROR: {e}")
+            return f"ERROR: {e}"
+    # See all current Tasks
+    else:
+        tasks = Task.query.order_by(Task.created).all()
+        return render_template("index.html", tasks=tasks)
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    task = Task.query.get_or_404(id)
+    try:
+        db.session.delete(task)
+        db.session.commit()
+        return redirect("/")
+    except Exception as e:
+        return f"Error deleting task: {e}"
+
+@app.route("/update/<int:id>", methods=["GET", "POST"])
+def update(id):
+    task = Task.query.get_or_404(id)
+    if request.method == "POST":
+        task.content = request.form["content"]
+        try:
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            return f"Error updating task: {e}"
+    else:
+        return render_template("update.html", task=task)
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
